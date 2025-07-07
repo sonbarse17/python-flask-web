e25ac29f4f3d614f09885f60894dca98d091448c
sonbarse17 <sushantsonbarse90@gmail.com>
2025-07-07 15:38:26 +0530

    fixed

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Dosto, welcome to My Repositoy! This is a Flask app. Enjoy! Created by Sushant Sonbarse'
+    return render_template('index.html')
 
 @app.route('/health')
 def health():
-    return 'Server is up and running'
+    return render_template('health.html')
