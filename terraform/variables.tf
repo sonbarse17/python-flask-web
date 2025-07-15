a2061ff936670e32941eabf8206b6eebab57c40c
sonbarse17 <sushantsonbarse90@gmail.com>
2025-07-15 11:19:08 +0530

    changed cluster version

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,7 +12,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "The Kubernetes version for the EKS cluster."
   type        = string
-  default     = "1.21"
+  default     = "1.30"
 }
 
 variable "instance_types" {
